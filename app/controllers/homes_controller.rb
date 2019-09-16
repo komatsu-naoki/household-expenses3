eb3257c6878c04a42c95abd13b0cebbdc14032db
komatsu-naoki <base.ball44.kn.2018@icloud.com>
2019-09-16 16:33:10 +0900

    支出5

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,23 +1,44 @@
 class HomesController < ApplicationController
 
  def index
-  current_user.spendings.each do |spending|
-    if spending.name == "食費" && Date.today <= spending.created_at && spending.created_at < Date.today + 1 
-    @range = Date.today.beginning_of_day..Date.today.end_of_day
-    @date = Spending.where(created_at: @range)
-    @sum1 = 0
-    @date.each do |spending_sum|
-    @sum1 += spending_sum.value.to_i
-    @sum2 = "円"
+  
+  @range = Date.today.beginning_of_day..Date.today.end_of_day
+    
+    @food0 = Spending.where(name: "食費").where(created_at: @range)
+    @food1 = 0
+    
+    @food0.each do |spending_sum|
+     if @food0 
+      @food1 += spending_sum.value.to_i
+      @food2 = "円"
+      @foodname = spending_sum.name 
+     end
     end
-  end
-end
 
-    current_user.spendings.each do |spending|
-    if spending.name == "食費" && Date.today <= spending.created_at && spending.created_at < Date.today + 1 
-      @name = spending.name 
+    @dailynecessities0 = Spending.where(name: "日用品").where(created_at: @range)
+    @dailynecessities1 = 0
+    
+    @dailynecessities0.each do |spending_sum|
+     if @dailynecessities0 
+      @dailynecessities1 += spending_sum.value.to_i
+      @dailynecessities2 = "円"
+      @dailynecessitiesname = spending_sum.name 
+     end
     end
+
+    @tex0 = Spending.where(name: "交通費").where(created_at: @range)
+    @tex1 = 0
+    
+    @tex0.each do |spending_sum|
+     if @tex0 
+      @tex1 += spending_sum.value.to_i
+      @tex2 = "円"
+      @texname = spending_sum.name 
+     end
+    end
+
+
+
   end
- end
 end
 
