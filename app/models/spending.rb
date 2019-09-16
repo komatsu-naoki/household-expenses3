07ce99e03aad276e532d208df63ed31098c302c7
komatsu-naoki <base.ball44.kn.2018@icloud.com>
2019-09-16 13:20:53 +0900

    支出4

--- app/models/spending.rb
+++ app/models/spending.rb
@@ -3,4 +3,11 @@ class Spending < ApplicationRecord
   has_many :users, through: :moneys
 
   validates :value, presence: true
+
+  def self.sumvalue
+     sum1 = Spending.where(name: "食費")
+     sum2 = sum1.select(:value)
+     sum3 = sum2.sum(:value)
+  end
+  
 end
